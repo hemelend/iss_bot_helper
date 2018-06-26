dba940b5c5809213e2f1132b7b68d002690cb0a0
Hugo Melendez <humelend@microsoft.com>
2018-06-25 18:21:06 -0600

    added welcome message with options to user

--- SFBot/Dialogs/RootDialog.cs
+++ SFBot/Dialogs/RootDialog.cs
@@ -2,30 +2,61 @@
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using SFBot.Properties;
+
 
 namespace SFBot.Dialogs
 {
     [Serializable]
     public class RootDialog : IDialog<object>
     {
-        public Task StartAsync(IDialogContext context)
+        //private Models.SFRequest SFRequest;
+
+
+        public async Task StartAsync(IDialogContext context)
         {
             context.Wait(MessageReceivedAsync);
 
-            return Task.CompletedTask;
+            //return Task.CompletedTask;
         }
 
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
         {
             var activity = await result as Activity;
 
-            // Calculate something for us to return
-            int length = (activity.Text ?? string.Empty).Length;
+            await this.WelcomeMessageAsync(context);
+            //context.Wait(MessageReceivedAsync);
+        }
 
-            // Return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+        private async Task WelcomeMessageAsync(IDialogContext context)
+        {
 
-            context.Wait(MessageReceivedAsync);
+            var reply = context.MakeMessage();
+
+            string[] options = MessageOptions();
+
+            reply.AddHeroCard(
+                Resources.RootDialog_Welcome_Message,
+                //Resources.RootDialog_Welcome_Subtitle,
+                options, null);
+
+            await context.PostAsync(reply);
+
+            context.Wait(this.OnOptionSelected);
+        }
+
+        private static string[] MessageOptions()
+        {
+            return new[]
+            {
+                Resources.RootDialog_Welcome_SFRequest,
+                Resources.RootDialog_Welcome_Solution_Helper
+            };
+        }
+
+        private Task OnOptionSelected(IDialogContext context, IAwaitable<object> result)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ SFBot/Models/SFRequest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Builder.FormFlow.Advanced;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SFBot.Models
+{
+    public class SFRequest
+    {
+         public enum AreaOptions
+        {
+            APAC, Canada, CentralAndEasternEurope, France, Germany, GreaterChina, India, Japan, Latam, MEA, UK, UnitedStates, WesternEurope
+        }
+        public AreaOptions? Area;
+
+        [Prompt]
+        public string CustomerName { get; set; }
+
+        [Prompt]
+        public string OpportunityID { get; set; }
+
+        [Prompt]
+        public bool Demo { get; set; }
+
+        [Prompt]
+        public DateTime StartTime { get; set; }
+
+        [Prompt]
+        public string Description { get; set; }
+
+        [Prompt]
+        public string SolutionArea { get; set; }
+
+
+        public static IForm<SFRequest> BuildRequestForm()
+        {
+            return new FormBuilder<SFRequest>()
+                .Field(new FieldReflector<SFRequest>(nameof(AreaOptions)))
+                .Field(nameof(CustomerName))
+                .Field(nameof(OpportunityID))
+                .Field(nameof(Demo))
+                .Field(nameof(StartTime))
+                .Field(nameof(Description))
+                .Field(nameof(SolutionArea))
+                .Build();
+        }
+    }
+}
